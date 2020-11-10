c6898f026c393b39dd106c3037bf054534f6ee74
charakaSamawry <62015653+charakaSamawry@users.noreply.github.com>
2020-11-10 17:59:07 +0000

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1141,11 +1141,11 @@
 #define X_BED_SIZE 235
 #define Y_BED_SIZE 235
 
-// Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
-#define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+// Travel limits (mm) after homing, corresponding to endstop positions. (??Not that important ccan be set from printer..)
+#define X_MIN_POS 0    // [                                                ]
+#define Y_MIN_POS 0    // [Home offsets - Corresponding to endstop position]
+#define Z_MIN_POS 0    // [            Can be set from screen..            ]
+#define X_MAX_POS X_BED_SIZE 
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 
